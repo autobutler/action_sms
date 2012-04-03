5719fc793eb57e5e128e67ad979d27a75003bfd4
Jens Henrik Hertz <nineus@gmail.com>
2012-04-03 12:59:30 +0200

    Make sure base is inside the ActionSms module.

--- lib/action_sms/base.rb
+++ lib/action_sms/base.rb
@@ -1,67 +1,69 @@
-# Implementation
-class Base < AbstractController::Base
-  include AbstractController::Rendering
-  include AbstractController::Layouts
-  include AbstractController::Helpers
-  include AbstractController::Translation
-  include AbstractController::AssetPaths
+module ActionSms
+  # Implementation
+  class Base < AbstractController::Base
+    include AbstractController::Rendering
+    include AbstractController::Layouts
+    include AbstractController::Helpers
+    include AbstractController::Translation
+    include AbstractController::AssetPaths
   
-  class << self
-    def respond_to?(method, include_private = false) #:nodoc:
-      super || action_methods.include?(method.to_s)
-    end
+    class << self
+      def respond_to?(method, include_private = false) #:nodoc:
+        super || action_methods.include?(method.to_s)
+      end
   
-  protected
+    protected
   
-    def method_missing(method, *args)
-      return super unless respond_to?(method)
-      new(method, *args).message
-    end
-  end
-  
-  attr_internal :message
-  
-  def initialize(method_name=nil, *args)
-    super()
-    @_message = SMS.new
-    process(method_name, *args) if method_name
-  end
-  
-  def process(*args) #:nodoc:
-    lookup_context.skip_default_locale!
-    super
-  end
-
-  def sms(headers={})
-    m = @_message
-
-    assignable = headers.except(:body)
-    assignable.each { |k, v| m[k] = v }
-    
-    m.body = gather_body(headers)
-    m
-  end
-  
-  def gather_body(headers)
-    responses = []
-    if headers[:body]
-      responses << headers.delete(:body)
-    else
-      templates_path = headers.delete(:template_path) || self.class.mailer_name
-      templates_name = headers.delete(:template_name) || action_name
-
-      each_template(templates_path, templates_name) do |template|
-        next unless template.mime_type.to_s == "text/plain"
-        self.formats = template.formats
-
-        responses << render(:template => template)
+      def method_missing(method, *args)
+        return super unless respond_to?(method)
+        new(method, *args).message
       end
     end
-    responses.join(" ")
-  end
   
-  def each_template(paths, name, &block) #:nodoc:
-    templates = lookup_context.find_all(name, Array.wrap(paths))
-    templates.uniq_by { |t| t.formats }.each(&block)
-  end
-end  
+    attr_internal :message
+  
+    def initialize(method_name=nil, *args)
+      super()
+      @_message = SMS.new
+      process(method_name, *args) if method_name
+    end
+  
+    def process(*args) #:nodoc:
+      lookup_context.skip_default_locale!
+      super
+    end
+
+    def sms(headers={})
+      m = @_message
+
+      assignable = headers.except(:body)
+      assignable.each { |k, v| m[k] = v }
+    
+      m.body = gather_body(headers)
+      m
+    end
+  
+    def gather_body(headers)
+      responses = []
+      if headers[:body]
+        responses << headers.delete(:body)
+      else
+        templates_path = headers.delete(:template_path) || self.class.mailer_name
+        templates_name = headers.delete(:template_name) || action_name
+
+        each_template(templates_path, templates_name) do |template|
+          next unless template.mime_type.to_s == "text/plain"
+          self.formats = template.formats
+
+          responses << render(:template => template)
+        end
+      end
+      responses.join(" ")
+    end
+  
+    def each_template(paths, name, &block) #:nodoc:
+      templates = lookup_context.find_all(name, Array.wrap(paths))
+      templates.uniq_by { |t| t.formats }.each(&block)
+    end
+  end  
+end
