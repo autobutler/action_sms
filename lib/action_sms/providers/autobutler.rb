e9173900d616d622dd07ab31950bef555e85c20f
Lars Sehested <lars@larssehested.com>
2020-01-08 15:53:42 +0100

    Add Autobutler SMS Gateway provider

--- lib/action_sms/providers/autobutler.rb
+++ lib/action_sms/providers/autobutler.rb
@@ -4,7 +4,7 @@ require 'rack'
 
 module ActionSms
   module Providers
-    class CoolSmsProvider < Provider
+    class AutobutlerProvider < Provider
       DELIVERED_OK = 'SMS sent'
       DELIVER_EXCEPTION = 'Exception: %s'
       SMS_GATEWAY_ERROR = 'SMS Gateway Error Code: %s'
@@ -14,30 +14,22 @@ module ActionSms
         all_ok = false
 
         begin
-          url = "https://api.linkmobility.dk/v2/message.json?apikey=#{ENV['COOLSMS_API_KEY']}"
-
-          unless message.phone_number.starts_with?('+')
-            phone_number = '+' + message.phone_number
-          end
-          from = message.from.blank? ? ActionSms.options[:from] : message.from
+          url = 'https://sms.autobutler.dk/api/v1/messages'
 
           body = {
             message: {
-              recipients: phone_number,
-              sender: from,
-              message: message.body,
-              format: FORMAT
+              to: message.phone_number,
+              body: message.body
             }
           }
 
           if message.status_report_url.present?
-            body[:message][:status] = true
-            body[:message][:statusurl] = message.status_report_url
+            body[:callback_url] = message.status_report_url
           end
 
-          response = RestClient.post(url, body.to_json, content_type: 'application/json; charset=utf-8')
+          response = RestClient.post(url, body, Authorization: "Bearer #{ENV['AUTOBUTLER_SMS_TOKEN']}")
 
-          all_ok = response.code == 201
+          all_ok = response.code == 200
           if all_ok
             parsed_response = JSON.parse(response.body)
             message.message_id = parsed_response['details']['batchid']
--- lib/action_sms/providers/test.rb
+++ lib/action_sms/providers/test.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module ActionSms
   module Providers
     class TestProvider < Provider
       def self.deliver(m)
-        m.after_send(true, "TestProvider: SMS sent.")
-        return true
+        m.after_send(true, 'TestProvider: SMS sent.')
+        true
       end
     end
   end
